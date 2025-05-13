e6d7db9f0be5c2b7c3c474ca0165374bb79e5dda
egide2021 <egihabu@gmail.com>
2025-05-13 13:42:16 -0400

    Add simple counting quiz script for Grade 1

---
+++ script.js
@@ -0,0 +1,8 @@
+function startGrade1Quiz() {
+  const question = prompt("How many apples are there? 🍎🍎🍎🍎🍎");
+  if (question === "5") {
+    alert("🎉 Correct! Great job!");
+  } else {
+    alert("❌ Oops! Try again.");
+  }
+}
